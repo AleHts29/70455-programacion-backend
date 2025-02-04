2240851f976bc234eb099ef3e237a533312f2ece
Alejandro Huertas <ahuertas@fi.uba.ar>
2025-02-03 22:34:40 -0300

    clase12

---
+++ Clase_12/testQueryMongo.js
@@ -0,0 +1,32 @@
+db.estudiantes.insertMany([
+    {
+        nombre: "Juan",
+        apellido: "Perez",
+        curso: "Matemáticas",
+        correo: "juan.perez@example.com"
+    },
+    {
+        nombre: "María",
+        apellido: "González",
+        curso: "Historia",
+        correo: "maria.gonzalez@example.com"
+    },
+    {
+        nombre: "Pedro",
+        apellido: "Sánchez",
+        curso: "Ciencias",
+        correo: "pedro.sanchez@example.com"
+    },
+    {
+        nombre: "Laura",
+        apellido: "Martínez",
+        curso: "Inglés",
+        correo: "laura.martinez@example.com"
+    },
+    {
+        nombre: "Ana",
+        apellido: "López",
+        curso: "Arte",
+        correo: "ana.lopez@example.com"
+    }
+])
